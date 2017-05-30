e31455386d6dc96617e48694409f9ff77ed08913
superficialss <superficialss@outlook.com>
2017-05-30 20:09:15 +0800

    两个队列实现栈

--- InterviewQuestion/QueueAndStack.h
+++ InterviewQuestion/QueueAndStack.h
@@ -120,6 +120,7 @@ void TestStackWithMin()
 }
 
 //使用两个栈实现一个队列
+//由于头和尾实现较为麻烦，则不实现了
 class CQueue
 {
 public:
@@ -144,6 +145,11 @@ public:
 		return s1.size() + s2.size();
 	}
 
+	bool Empty()
+	{
+		return s1.empty() && s2.empty();
+	}
+
 protected:
 	stack<int> s1;
 	stack<int> s2;
@@ -157,16 +163,109 @@ void TestCQueue()
 	q.Push(3);
 	q.Push(4);
 	q.Push(5);
-	cout << "Size:" << q.Size() << endl;
-	q.Pop();
-	cout << "Size:" << q.Size() << endl;
-	q.Pop();	
-	cout << "Size:" << q.Size() << endl;
-	q.Pop();	
-	cout << "Size:" << q.Size() << endl;
-	q.Pop();
-	cout << "Size:" << q.Size() << endl;
-	q.Pop();
-	cout << "Size:" << q.Size() << endl;
+
+	while (!q.Empty())
+	{
+		cout << "Size:" << q.Size() << endl;
+		q.Pop();
+	}
 }
+
+// 使用两个队列实现一个栈
+class CStack
+{
+public:
+	void Push(const int& x)
+	{
+		if (!q1.empty())
+		{
+			q2.push(x);
+			while (!q1.empty())
+			{
+				q2.push(q1.front());
+				q1.pop();
+			}
+		}
+		else
+		{
+			q1.push(x);
+			while (!q2.empty())
+			{
+				q1.push(q2.front());
+				q2.pop();
+			}
+		}
+	}
+
+	void Pop()
+	{
+		if (q1.empty() && q2.empty())
+		{
+			cout << "The Stack Is Empty!" << endl;
+			exit(1);
+		}
+		else if (q1.empty())
+		{
+			q2.pop();
+		}
+		else
+		{
+			q1.pop();
+		}
+	}
+
+	int Size()
+	{
+		return q1.size() + q2.size();
+	}
+
+	int Top()
+	{
+		assert(!q1.empty() || !q2.empty());
+		if (q1.empty())
+		{
+			return q2.front();
+		}
+		else
+		{
+			return q1.front();
+		}
+	}
+
+	bool Empty()
+	{
+		return q1.empty() && q2.empty();
+	}
+
+protected:
+	queue<int> q1;
+	queue<int> q2;
+};
+
+void TestCStack()
+{
+	CStack s;
+	s.Push(1);
+	s.Push(2);
+	s.Push(3);
+	s.Push(4);
+	s.Push(5);
+
+	cout << "Size:" << s.Size() << endl;
+
+	while (!s.Empty())
+	{
+		cout << s.Top() << " ";
+		s.Pop();
+	}
+	cout << endl;
+}
+
+
+// 元素出栈、入栈顺序的合法性。如入栈的序列（1, 2, 3, 4, 5），出栈序列为（4, 5, 3, 2, 1）
+
+
+
+// 一个数组实现两个栈
+
 
--- InterviewQuestion/Test.cpp
+++ InterviewQuestion/Test.cpp
@@ -10,6 +10,7 @@ using namespace std;
 int main()
 {
 	//TestStackWithMin();
-	TestCQueue();
+	//TestCQueue();
+	TestCStack();
 	return 0;
 }
